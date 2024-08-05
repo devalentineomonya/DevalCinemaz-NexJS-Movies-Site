fa4890859a452de96da0ccad25a1502ad5f46624
Valentine Omonya <valomosh254@gmail.com>
2024-08-05 19:17:05 +0300

    Added error handling

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,10 +11,10 @@ module.exports = {
     extend: {
       fontFamily: {
         caveats: ['Caveat', 'cursive'],
-        stencil: ["stencil", "static"],
-        Inter: ["Inter"],
-        Dosis: ["Dosis"],
-        Arial: ["Arial"]
+        stencil: ['stencil', 'static'],
+        Inter: ['Inter'],
+        Dosis: ['Dosis'],
+        Arial: ['Arial'],
       },
       colors: {
         customDark: "#050505",
@@ -23,21 +23,11 @@ module.exports = {
         customOrange: {
           100: "#937b23",
           200: "#816c21",
-          300: "#71631b"
+          300: "#71631b",
         },
         customWhite: "#feffff",
       },
-
-      plugins: [],
-      purge: {
-        enabled: process.env.NODE_ENV === "production",
-        content: [
-          "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-          "./components/**/*.{js,ts,jsx,tsx,mdx}",
-          "./app/**/*.{js,ts,jsx,tsx,mdx}",
-        ],
-      },
-    }
+    },
   },
+  plugins: [],
 }
-
